dd289664573885de2b5b3f96a354c713a41faec5
Eduardo Roveroto <eduardo.roverote@eaportal.org>
2024-11-21 22:03:04 +0000

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+ async function verifyToken() {
+    const token = localStorage.getItem("token")
+
+    if (!token) {
+        window.location.href = "./pages/login/login.html"
+        return
+    }
+
+    const response = await fetch("http://localhost:3000/verify", {
+        headers: {
+            "Authorization": token
+        }
+        
+    }).then(response => response.json())
+    
+}
+
+verifyToken()
